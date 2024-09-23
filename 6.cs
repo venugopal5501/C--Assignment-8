d11b4aa8dda81f1f277a17009c0eb2b5c4b7af7b
venugopal5501 <venugopal14082002@gmail.com>
2024-09-23 17:33:41 +0530

    space

---
+++ 6.cs
@@ -0,0 +1,28 @@
+using System;
+delegate string spa(string n);
+class space
+{
+    public static string spaces(string n)
+    {
+        int count = 0;
+        foreach (char c in n)
+        {
+            if (c == ' ')
+            {
+                count++;
+            }
+        }
+        Console.Write(count);
+        return n;
+
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the Word or sentence : ");
+        string sentence = Console.ReadLine();
+        spa s = new spa(spaces);
+        string sc;
+        sc = s(sentence);
+
+    }
+}
