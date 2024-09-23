764d4869e9af4239375e74a5fb151c4b37d877ae
venugopal5501 <venugopal14082002@gmail.com>
2024-09-23 16:54:10 +0530

    2

---
+++ 2.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+class read{
+    public static void Main(string []args){
+        FileStream fs=new FileStream("d://out1.txt",FileMode.OpenOrCreate, FileAccess.Read);
+        StreamReader sr=new StreamReader(fs);
+        string reading=sr.ReadToEnd();
+        sr.Close();
+        fs.Close();
+        Console.WriteLine(reading);
+    }
+}
